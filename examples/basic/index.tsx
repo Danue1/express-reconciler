08d38fb4b910fed5b4ad83b60c21797ed8bbece4
Danuel <public.danuel@gmail.com>
2021-02-09 13:12:24 +0900

    Update code style of examples

--- examples/basic/index.tsx
+++ examples/basic/index.tsx
@@ -1,31 +1,33 @@
 import Express, { ParameterContext, ResponseBuilder } from "../../src";
 
-const print = (port: number): void => {
-  console.clear();
-  console.log(`[Express Reconciler] Server Running on ${port} Port.`);
-};
-
-const hello = (): ResponseBuilder => {
-  return ResponseBuilder.of().body("Hello, World!");
-};
-
-interface Parameter {
-  readonly name: string;
-}
-
-const name = ({ params: { name } }: ParameterContext<Parameter>): ResponseBuilder => {
-  return ResponseBuilder.of().body(`Hello, ${name}!`);
-};
+Express.start(<App port={8080} />);
 
 interface Props {
   readonly port: number;
 }
 
-const App = ({ port }: Props) => (
-  <app port={port} onListen={print}>
-    <get path="/" controller={hello} />
-    <get path="/:name" controller={name} />
-  </app>
-);
+function App({ port }: Props) {
+  const print = (port: number): void => {
+    console.clear();
+    console.log(`[Express Reconciler] Server Running on ${port} Port.`);
+  };
 
-Express.start(<App port={8080} />);
+  const hello = (): ResponseBuilder => {
+    return ResponseBuilder.of().body("Hello, World!");
+  };
+
+  interface Parameter {
+    readonly name: string;
+  }
+
+  const name = ({ params: { name } }: ParameterContext<Parameter>): ResponseBuilder => {
+    return ResponseBuilder.of().body(`Hello, ${name}!`);
+  };
+
+  return (
+    <app port={port} onListen={print}>
+      <get path="/" controller={hello} />
+      <get path="/:name" controller={name} />
+    </app>
+  );
+}
--- examples/router/index.tsx
+++ examples/router/index.tsx
@@ -1,39 +1,45 @@
 import Express, { ResponseBuilder } from "../../src";
 
-const print = (port: number): void => {
-  console.clear();
-  console.log(`[Express Reconciler] Server Running on ${port} Port.`);
-};
+Express.start(<App port={8080} />);
 
 interface Props {
   readonly port: number;
 }
 
-const App = ({ port }: Props) => (
-  <app port={port} onListen={print}>
-    <GraphQLRouter />
-    <VersionRouter />
-  </app>
-);
+function App({ port }: Props) {
+  const print = (): void => {
+    console.clear();
+    console.log(`[Express Reconciler] Server Running on ${port} Port.`);
+  };
 
-const graphql = (): ResponseBuilder => {
-  return ResponseBuilder.of().body(`Hello, GraphQL Broadcast!`);
+  return (
+    <app port={port} onListen={print}>
+      <GraphQLRouter />
+      <VersionRouter />
+    </app>
+  );
+}
+
+const GraphQLRouter = () => {
+  const graphql = (): ResponseBuilder => {
+    return ResponseBuilder.of().body(`Hello, GraphQL Broadcast!`);
+  };
+
+  return (
+    <router path="/graphql">
+      <get path="/broadcast" controller={graphql} />
+    </router>
+  );
 };
 
-const GraphQLRouter = () => (
-  <router path="/graphql">
-    <get path="/broadcast" controller={graphql} />
-  </router>
-);
+const VersionRouter = () => {
+  const version = (): ResponseBuilder => {
+    return ResponseBuilder.of().body("Hello, Version Broadcast!");
+  };
 
-const version = (): ResponseBuilder => {
-  return ResponseBuilder.of().body("Hello, Version Broadcast!");
+  return (
+    <router path="/version">
+      <get path="/broadcast" controller={version} />
+    </router>
+  );
 };
-
-const VersionRouter = () => (
-  <router path="/version">
-    <get path="/broadcast" controller={version} />
-  </router>
-);
-
-Express.start(<App port={8080} />);
